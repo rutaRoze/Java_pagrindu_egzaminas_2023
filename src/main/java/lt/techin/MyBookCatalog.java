2bfa97966307a9108a300b9e440f3f78af4d91bd
Rūta Rožėnė <rozene.ruta@gmail.com>
2023-11-28 13:50:16 +0200

    Method getBookByIsbn implemented

--- src/main/java/lt/techin/MyBookCatalog.java
+++ src/main/java/lt/techin/MyBookCatalog.java
@@ -4,24 +4,59 @@ import lt.techin.library.Book;
 import lt.techin.library.BookCatalog;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MyBookCatalog implements BookCatalog {
+
+    private Book book;
+    private Map<String, Book> booksList;
+
+    public MyBookCatalog() {
+        this.book = new Book();
+        this.booksList = new HashMap<>();
+    }
+
     @Override
     public void addBook(Book book) {
+        if (book == null || book.getIsbn() == null || book.getTitle() == null ||
+                book.getIsbn().isEmpty() || book.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("Invalid book data");
+        }
 
+        if (!booksList.containsKey(book.getIsbn())) {
+            booksList.put(book.getIsbn(), book);
+        }
     }
 
     @Override
-    public Book getBookByIsbn(String s) {
-        return null;
+    public Book getBookByIsbn(String isbn) {
+        Book searchedBook = booksList.get(isbn);
+
+        return searchedBook;
     }
 
     @Override
-    public List<Book> searchBooksByAuthor(String s) {
-        return null;
+    public List<Book> searchBooksByAuthor(String author) {
+
+        List<Book> books = new ArrayList<>();
+
+        for (Book b : booksList.values()) {
+            if(b.getAuthors().contains(author)) {
+                books.add(b);
+            }
+        }
+
+
+//        List<Book> books = booksList.values()
+//                .stream()
+//                .flatMap(b -> b.getAuthors().stream())
+//                .filter(b -> b.equals(author)).toList();
+        return books;
     }
 
     @Override
